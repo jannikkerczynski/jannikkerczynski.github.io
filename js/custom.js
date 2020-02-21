62ed0994d67dd1e941af694344a73dd27887f26c
Jannik Kerczynski <you@example.com>
2020-02-21 02:14:16 +0100

    DE PT EN Index

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,88 @@
 $(function() {});
+
+$(window).scroll(function() {
+  if (window.scrollY >= 200) {
+    $("nav").css("background-color", "#352E74");
+  } else {
+    $("nav").css("background-color", "#ffffff00");
+  }
+});
+
+$(".buttonSocialMedia").hover(
+  function() {
+    setTimeout(() => {
+      $("#linkedIn").css("opacity", "1");
+    }, 200);
+    setTimeout(() => {
+      $("#instagram").css("opacity", "1");
+    }, 300);
+    setTimeout(() => {
+      $("#facebook").css("opacity", "1");
+    }, 400);
+  },
+  function() {
+    $("#facebook").css("opacity", "0");
+    setTimeout(() => {
+      $("#instagram").css("opacity", "0");
+    }, 50);
+    setTimeout(() => {
+      $("#linkedIn").css("opacity", "0");
+    }, 100);
+  }
+);
+
+$(".buttonSocialMediaPT").hover(
+  function() {
+    setTimeout(() => {
+      $("#linkedIn").css("opacity", "1");
+    }, 200);
+    setTimeout(() => {
+      $("#instagram").css("opacity", "1");
+    }, 300);
+    setTimeout(() => {
+      $("#facebook").css("opacity", "1");
+    }, 400);
+  },
+  function() {
+    $("#facebook").css("opacity", "0");
+    setTimeout(() => {
+      $("#instagram").css("opacity", "0");
+    }, 50);
+    setTimeout(() => {
+      $("#linkedIn").css("opacity", "0");
+    }, 100);
+  }
+);
+$(".buttonSocialMediaDE").hover(
+  function() {
+    setTimeout(() => {
+      $("#linkedIn").css("opacity", "1");
+    }, 200);
+    setTimeout(() => {
+      $("#instagram").css("opacity", "1");
+    }, 300);
+    setTimeout(() => {
+      $("#facebook").css("opacity", "1");
+    }, 400);
+  },
+  function() {
+    $("#facebook").css("opacity", "0");
+    setTimeout(() => {
+      $("#instagram").css("opacity", "0");
+    }, 50);
+    setTimeout(() => {
+      $("#linkedIn").css("opacity", "0");
+    }, 100);
+  }
+);
+
+$(".navLanguage").click(function() {
+  $(".languageList").css("height", "100px");
+  $(".languageList").css("opacity", "1");
+});
+$("html").click(function(e) {
+  if (!$(e.target).hasClass("navLanguage")) {
+    $(".languageList").css("height", "0");
+    $(".languageList").css("opacity", "0");
+  }
+});
